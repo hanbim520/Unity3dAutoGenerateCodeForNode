5eea4c89387d1e0dfa14e1513442bbd549542ef9
zhanghaijun03 <710605420@qq.com>
2017-09-18 13:36:50 +0800

    add base window

---
+++ UIHelper/Assets/Scripts/IWindowAPI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWindowAPI {
+    void Init();
+    void Open();
+    void Close();
+    void Hide();
+    void Destroy();
+    void Refresh();
+}
---
+++ UIHelper/Assets/Scripts/WindowBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowBase : MonoBehaviour, IWindowAPI
+{
+
+    public virtual void Init() { }
+    public virtual void Open() { }
+    public virtual void Close() { }
+    public virtual void Hide() { }
+    public virtual void Destroy() { }
+    public virtual void Refresh() { }    
+}
