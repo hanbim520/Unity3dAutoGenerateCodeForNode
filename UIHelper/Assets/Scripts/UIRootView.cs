91416f415809e6cbdfcac82c0782c05f3b18172c
zhanghaijun03 <710605420@qq.com>
2017-09-18 17:28:02 +0800

    add testview

---
+++ UIHelper/Assets/Scripts/UIRootView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRootView : UIRootTest {
+
+    public override void Init()
+    {
+        base.Init();
+    }
+    public override void Open()
+    {
+        base.Open();
+    }
+
+    public override void Destroy()
+    {
+        base.Destroy();
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+}
