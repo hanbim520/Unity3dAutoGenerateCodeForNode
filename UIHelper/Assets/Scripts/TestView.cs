2dba8ee34f17c5b1d94dab4182ff8e09f86c304c
zhanghaijun03 <710605420@qq.com>
2017-09-18 15:58:45 +0800

    add test code

---
+++ UIHelper/Assets/Scripts/TestView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestView : ImageRoot {
+
+    public override void Init()
+    {
+        base.Init();
+    }
+    public override void Open()
+    {
+        base.Open();
+    }
+    public override void Destroy()
+    {
+        base.Destroy();
+    }
+}
